a8d7dff68b1fca362a2b46f910ca20bf844117f6
Deepak <deepak@DEEPAKs-MacBook-Air.local>
2016-10-30 21:36:16 +0530

    Updated Files

--- built/tunes.js
+++ built/tunes.js
@@ -1,36 +1,35 @@
 define(["require", "exports", "knockout"], function (require, exports, ko) {
     "use strict";
+    ;
     var TunesViewModel = (function () {
-        function TunesViewModel() {
+        function TunesViewModel(data) {
             var _this = this;
             this.path = '/photos/';
-            this.data = {
-                "Id": 1001,
-                "fname": "guitar3.jpg",
-                "path": "/photos/guitar3.jpg",
-                "SalePrice": 10485.00,
-                "ShortDesc": "Taylor ACE",
-                "Description": "Taylor ACE made in INDIA",
-                "Likes": "Nagendra Prahalad"
-            };
-            this.likes = ko.observable(this.data.Likes);
-            this.shortDesc = ko.observable(this.data.ShortDesc);
-            this.salePrice = ko.observable(this.data.SalePrice);
-            this.description = ko.observable(this.data.Description);
+            this.likes = ko.observable(data.Likes);
+            this.shortDesc = ko.observable(data.ShortDesc);
+            this.salePrice = ko.observable(data.SalePrice);
+            this.description = ko.observable(data.Description);
             this.formatCurrency = ko.computed({
                 read: function () {
-                    return "$" + _this.data.SalePrice.toFixed(2);
+                    return "$" + data.SalePrice.toFixed(2);
                 }
             });
             this.photoURL = ko.computed({
                 read: function () {
-                    return _this.path + _this.data.fname;
+                    return _this.path + data.fname;
                 }
             });
         }
         return TunesViewModel;
     }());
     // Bind the ViewModel to the View using knockout
-    ko.applyBindings(new TunesViewModel());
+    ko.applyBindings(new TunesViewModel({
+        "Id": 1001,
+        "fname": "guitar3.jpg",
+        "SalePrice": 12485.00,
+        "ShortDesc": "Taylor ACE",
+        "Description": "Taylor ACE made in Germany",
+        "Likes": "Nagendra Prahalad"
+    }));
 });
 //# sourceMappingURL=tunes.js.map
--- src/tunes.ts
+++ src/tunes.ts
@@ -1,5 +1,14 @@
 /* Copyright 2016 Springwiz Solutions Private Limited */
 import * as ko from "knockout";
+export interface Data {
+    "Id": number,
+    "fname": string,
+    "SalePrice": number,
+    "ShortDesc": string,
+    "Description": string,
+    "Likes": string
+};
+
 class TunesViewModel {
     likes: KnockoutObservable<string>
     shortDesc: KnockoutObservable<string>
@@ -9,29 +18,20 @@ class TunesViewModel {
     photoURL: KnockoutComputed<string>
 
     path = '/photos/';
-    data = {
-        "Id": 1001,
-        "fname": "guitar3.jpg",
-        "path": "/photos/guitar3.jpg",
-        "SalePrice": 10485.00,
-        "ShortDesc": "Taylor ACE",
-        "Description": "Taylor ACE made in INDIA",
-        "Likes": "Nagendra Prahalad"
-    };
 
-    constructor() {
-        this.likes = ko.observable(this.data.Likes);
-        this.shortDesc = ko.observable(this.data.ShortDesc);
-        this.salePrice = ko.observable(this.data.SalePrice);
-        this.description = ko.observable(this.data.Description);
+    constructor(data: Data) {
+        this.likes = ko.observable(data.Likes);
+        this.shortDesc = ko.observable(data.ShortDesc);
+        this.salePrice = ko.observable(data.SalePrice);
+        this.description = ko.observable(data.Description);
         this.formatCurrency = ko.computed({
             read: () => {
-                return "$" + this.data.SalePrice.toFixed(2);
+                return "$" + data.SalePrice.toFixed(2);
             }
         });
         this.photoURL = ko.computed({
             read: () => {
-                return this.path + this.data.fname;
+                return this.path + data.fname;
             }
         });
 
@@ -39,4 +39,11 @@ class TunesViewModel {
 }
 
 // Bind the ViewModel to the View using knockout
-ko.applyBindings(new TunesViewModel());
+ko.applyBindings(new TunesViewModel({
+        "Id": 1001,
+        "fname": "guitar3.jpg",
+        "SalePrice": 12485.00,
+        "ShortDesc": "Taylor ACE",
+        "Description": "Taylor ACE made in Germany",
+        "Likes": "Nagendra Prahalad"
+    }));
