670c4231b402b494d6dbb6be858ef061e788b5fb
Deepak <deepak@DEEPAKs-MacBook-Air.local>
2016-11-01 20:55:52 +0530

    Removed built directory

--- built/require-config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "knockout": "bower_components/knockout/dist/knockout",
-        "tunesmodel": "built/tunesmodel"
-    }
-});
-//# sourceMappingURL=require-config.js.map
