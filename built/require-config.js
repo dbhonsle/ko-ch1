4e62998d79fd89b06f8c47d8de5a57b60df7697c
Deepak Bhonsle <dbhonsle@rewdale.com>
2016-11-10 03:42:03 +0000

    Added support for typescript type-chcek

---
+++ built/require-config.js
@@ -0,0 +1,7 @@
+require.config({
+    paths: {
+        "knockout": "bower_components/knockout/dist/knockout",
+        "tunesmodel": "built/tunesmodel"
+    }
+});
+//# sourceMappingURL=require-config.js.map
